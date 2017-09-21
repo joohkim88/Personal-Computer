1dc72c9d9bc6d0d8ae8ade579e2fcac173ddc1ca
joohkim88 <joohkim88@gmail.com>
2017-09-21 20:49:48 +0900

    Update ubuntu-setup.sh

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -3,7 +3,7 @@
 
 # Seadrive
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 8756C4F765C9AC3CB6B85D62379CE192D401AB61 ;
-echo deb http://deb.seadrive.org jessie main | tee /etc/apt/sources.list.d/seafile.list ;
+echo deb http://deb.seadrive.org zesty main | tee /etc/apt/sources.list.d/seafile.list ;
 
 # Nextcloud
 add-apt-repository ppa:nextcloud-devs/client ;
