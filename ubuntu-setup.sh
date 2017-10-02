cf5e6bf1bb2c8164539cad93799416dd564ca2b5
joohkim88 <joohkim88@gmail.com>
2017-10-03 01:08:02 +0900

    Update ubuntu-setup.sh

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -6,7 +6,7 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 8756C4F765C9AC
 echo deb http://deb.seadrive.org zesty main | tee /etc/apt/sources.list.d/seafile.list ;
 
 # Seafile
-add-apt-repository ppa:seafile/seafile-client ;
+# add-apt-repository ppa:seafile/seafile-client ;
 
 # Nextcloud
 add-apt-repository ppa:nextcloud-devs/client ;
@@ -18,9 +18,12 @@ echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.
 # Papirus Theme
 add-apt-repository ppa:papirus/papirus ;
 
+# Arc Theme
+echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' > /etc/apt/sources.list.d/arc-theme.list ;
+
 apt update ;
 apt dist-upgrade -y ;
-apt install -y git vim fish evolution tlp guake gnome-sushi nextcloud-client seafile-gui papirus-icon-theme arc-theme fcitx fcitx-hangul sublime-text vlc mpv network-manager-openvpn-gnome gnome-tweak-tool ;
+apt install -y git vim fish evolution tlp guake gnome-sushi nextcloud-client seadrive-gui redshift papirus-icon-theme arc-theme fcitx fcitx-hangul sublime-text vlc mpv network-manager-openvpn-gnome unity-tweak-tool ;
 apt purge -y thunderbird ;
 apt autoremove -y ;
 
