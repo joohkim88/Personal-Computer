93b3b6728736a32083e1f7e4b8e1802b0608c251
joohkim88 <joohkim88@gmail.com>
2017-10-03 01:08:18 +0900

    Update ubuntu-setup.sh

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -3,7 +3,7 @@
 
 # Seadrive
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 8756C4F765C9AC3CB6B85D62379CE192D401AB61 ;
-echo deb http://deb.seadrive.org zesty main | tee /etc/apt/sources.list.d/seafile.list ;
+echo deb http://deb.seadrive.org xenial main | tee /etc/apt/sources.list.d/seafile.list ;
 
 # Seafile
 # add-apt-repository ppa:seafile/seafile-client ;
