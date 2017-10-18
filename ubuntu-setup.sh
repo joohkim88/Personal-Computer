715bc235d49ab590440e964084e96ebd0d18f2ef
joohkim88 <joohkim88@gmail.com>
2017-10-19 01:35:15 +0900

    Update ubuntu-setup.sh

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -2,8 +2,8 @@
 # To be run by superuser
 
 # Seadrive
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 8756C4F765C9AC3CB6B85D62379CE192D401AB61 ;
-echo deb http://deb.seadrive.org artful main | tee /etc/apt/sources.list.d/seafile.list ;
+# apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 8756C4F765C9AC3CB6B85D62379CE192D401AB61 ;
+# echo deb http://deb.seadrive.org artful main | tee /etc/apt/sources.list.d/seafile.list ;
 
 # Seafile
 add-apt-repository ppa:seafile/seafile-client ;
@@ -15,7 +15,7 @@ add-apt-repository ppa:nextcloud-devs/client ;
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add - ;
 echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list ;
 
-# Papirus Theme
+# Papirus Icons
 add-apt-repository ppa:papirus/papirus ;
 
 # Arc Theme
@@ -26,7 +26,7 @@ add-apt-repository ppa:papirus/papirus ;
 
 apt update ;
 apt dist-upgrade -y ;
-apt install -y git vim fish evolution tlp guake gnome-sushi nextcloud-client seafile-gui papirus-icon-theme arc-theme fcitx fcitx-hangul sublime-text vlc network-manager-openvpn-gnome gnome-tweak-tool steam ;
+apt install -y git vim fish evolution tlp guake gnome-sushi nextcloud-client seadrive-gui papirus-icon-theme fcitx fcitx-hangul sublime-text vlc network-manager-openvpn-gnome unity-tweak-tool steam ;
 apt purge -y thunderbird ;
 apt autoremove -y ;
 
